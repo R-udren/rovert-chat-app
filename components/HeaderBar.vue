6ae4778fc46965d4ca92236dc30f2539108377d1
Robert <54269637+R-udren@users.noreply.github.com>
2025-01-26 23:37:06 +0200

    neforse

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -1,47 +1,42 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 const user = useSupabaseUser();
 </script>
 
 <template>
-  <header>
-    <h1>My App</h1>
-    <nav>
-      <ul>
-        <li>
-          <ULink to="/">Home</ULink>
-        </li>
-        <template v-if="!user">
-          <li>
-            <ULink to="/login">Login</ULink>
-          </li>
-          <li>
-            <ULink to="/register">Register</ULink>
-          </li>
-        </template>
-        <template v-else>
-          <li>
-            <ULink to="/profile">Profile</ULink>
-          </li>
-        </template>
-        <ColorModeButton/>
-      </ul>
-    </nav>
+  <header class="flex items-center justify-between px-4 py-3 shadow-sm">
+    <div class="flex items-center space-x-4">
+      <ULink class="px-3 py-1 rounded-full font-bold text-xl tracking-wider" to="/">
+        ChatApp
+      </ULink>
+    </div>
+    <div class="flex items-center space-x-4">
+      <nav>
+        <ul class="flex space-x-4">
+          <template v-if="!user">
+            <li>
+              <UButton
+                  class="px-3 py-1"
+                  color="primary"
+                  label="Login"
+                  size="sm"
+                  to="/login"
+                  variant="solid"
+              />
+            </li>
+          </template>
+          <template v-else>
+            <li>
+              <ULink
+                  class="hover:text-blue-500 transition"
+                  to="/profile"
+              >
+                Profile
+              </ULink>
+            </li>
+          </template>
+        </ul>
+      </nav>
+      <ColorModeButton/>
+    </div>
   </header>
 </template>
-
-<style scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px;
-}
-
-nav ul {
-  display: flex;
-  gap: 10px;
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-</style>
