f9f5dc155c5809783b521050ce900343fce51dd4
Robert <54269637+R-udren@users.noreply.github.com>
2025-02-14 00:05:05 +0200

    Refactor ColorModeButton and HeaderBar components for improved code consistency and formatting

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -5,7 +5,10 @@ const user = useSupabaseUser();
 <template>
   <header class="flex items-center justify-between px-4 py-3 shadow-sm">
     <div class="flex items-center">
-      <ULink class="px-3 py-1 rounded-full font-bold text-xl tracking-wider" to="/">
+      <ULink
+        class="px-3 py-1 rounded-full font-bold text-xl tracking-wider"
+        to="/"
+      >
         ChatApp
       </ULink>
     </div>
@@ -15,27 +18,24 @@ const user = useSupabaseUser();
           <template v-if="!user">
             <li>
               <UButton
-                  color="primary"
-                  label="Sign In"
-                  size="lg"
-                  to="/login"
-                  variant="outline"
+                color="primary"
+                label="Sign In"
+                size="lg"
+                to="/login"
+                variant="outline"
               />
             </li>
           </template>
           <template v-else>
             <li>
-              <ULink
-                  class="hover:underline transition"
-                  to="/profile"
-              >
+              <ULink class="hover:underline transition" to="/profile">
                 Profile
               </ULink>
             </li>
           </template>
         </ul>
       </nav>
-      <ColorModeButton/>
+      <ColorModeButton />
     </div>
   </header>
 </template>
