2dda2c2baa779d9ab1ec889feeb3e343029729df
Robert <54269637+R-udren@users.noreply.github.com>
2025-02-20 11:04:00 +0200

    Create roadmap.vue

---
+++ pages/roadmap.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="flex flex-col items-center">
+  <h1 class="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-tr from-[var(--ui-primary)] to-[var(--ui-secondary)]">
+      Roadmap
+  </h1>
+  <main>
+    <div class="mt-4 text-lg">
+      <p>Let's think...</p>
+    </div>
+  </main>
+</div>
+</template>
