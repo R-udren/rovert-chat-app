684ac9a76ac2eef0fdea92186351df97b76365a7
Robert <54269637+R-udren@users.noreply.github.com>
2025-02-21 12:47:33 +0200

    Refactor layout component to use main tag and fix formatting

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <HeaderBar/>
-  <div class="flex flex-col items-center justify-center">
-    <slot/>
-  </div>
+  <HeaderBar />
+  <main class="flex flex-col items-center justify-center">
+    <slot />
+  </main>
 </template>
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,18 +1,18 @@
 export default defineNuxtRouteMiddleware((to, _from) => {
-    const user = useSupabaseUser()
+  const user = useSupabaseUser();
 
-    const toast = useToast()
+  const toast = useToast();
 
-    if (!user.value) {
-        navigateTo('/login')
-        toast.add({
-            title: 'Unauthorized',
-            description: 'You must be logged in to access this page',
-            color: 'error',
-            duration: 5000,
-        })
-        return false
-    }
+  if (!user.value) {
+    navigateTo("/login");
+    toast.add({
+      title: "Unauthorized",
+      description: "You must be logged in to access this page: " + to.path,
+      color: "error",
+      duration: 5000,
+    });
+    return false;
+  }
 
-    return true
-})
+  return true;
+});
